a291719dbe31a871617236e24ee5cdf06caf24eb
Hrabovsky Marián <hrabocsu@caesar.elte.hu>
2016-10-10 20:44:02 +0200

    SZNY-8 load/index: Alkalmazottak betöltésére szolgáló nézet létrehozva

---
+++ views/load/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/*********************************************
+*  ALKALMAZOTTAK BETÖLTÉSÉRE SZOLGÁLÓ NÉZET  *
+*********************************************/
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Alkalmazottak betöltése';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        Húzza ide a betölteni kívánt fájlt!
+    </p>
+
+</div>
