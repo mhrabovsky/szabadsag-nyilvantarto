0160ff2a6b998d6398025ddfdaa5c10215b7c03d
Hrabovsky Marián <hrabocsu@caesar.elte.hu>
2016-10-10 20:38:49 +0200

    SZNY-7 LoadController: Alkalmazottak betöltésére szolgáló vezérlő létrehozva

---
+++ controllers/LoadController.php
@@ -0,0 +1,67 @@
+<?php
+
+/***********************************************
+*  ALKALMAZOTTAK BETÖLTÉSÉRE SZOLGÁLÓ VEZÉRLŐ  *
+***********************************************/
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+
+class LoadController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Kezdőlap megjelenítése.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->renderPartial('index');
+    }
+}
